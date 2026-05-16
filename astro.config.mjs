a9062458949e6814d3290d336a9745c3f6ef8f2b
Jibin Jose <jibinjose884@gmail.com>
2026-05-16 07:53:04 +0530

    fix GitHub Pages site/base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jibin7jose.github.io',
-  base: '/Jibin.dev',
+  site: 'https://jibin7jose.github.io/Jibin.dev',
+  base: '/Jibin.dev/',
 });
